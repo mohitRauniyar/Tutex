7d7c97d931e86d086a6acdb4dc7df351ea34dff8
Purab Sen <98190barup@gmail.com>
2025-04-26 16:43:00 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { configDotenv } from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import authenticateUser from "./middlewares/authenticator.js";
 import loginRoute from "./routes/authRoute.js"
 import registrationRoute from "./routes/registrationRoute.js"
@@ -14,6 +15,8 @@ configDotenv();
 const app = express();
 const port = process.env.PORT;
 
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
