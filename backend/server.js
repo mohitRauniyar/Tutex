2890c7d1ad3d34ea6d9e96537ab79993511122ab
Purab Sen <98190barup@gmail.com>
2025-04-26 19:54:55 +0530

    deployment fix 3

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 const port = process.env.PORT;
 
 
-app.use(cors());
+app.use(cors({origin:'*',credentials:true}));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
